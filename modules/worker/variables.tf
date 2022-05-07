1e1aebe08cdfc1afc1a5850661504959d72ae50e
shaytac <shaytac@newsamerica.com>
2022-05-07 12:42:27 -0400

    disable public subnets

--- modules/worker/variables.tf
+++ modules/worker/variables.tf
@@ -73,10 +73,10 @@ variable "min_size" {
   type        = number
 }
 
-variable "public_subnets" {
-  description = "List of public subnets"
-  type        = list(string)
-}
+# variable "public_subnets" {
+#   description = "List of public subnets"
+#   type        = list(string)
+# }
 variable "private_subnets" {
   description = "List of private subnets"
   type        = list(string)
